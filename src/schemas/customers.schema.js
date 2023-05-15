a59af2cf67f86f0f6c69265dc10769c442e81072
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-05-15 01:02:11 -0300

    Requirement: crud of customers - completed #2

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -3,6 +3,6 @@ import joi from 'joi';
 export const registerCustomerSchema = joi.object({
     name: joi.string().required().trim(),
     phone: joi.string().trim().min(10).max(11),
-    cpf: joi.string().min(0).length(11).required(),
+    cpf: joi.string().pattern(/^\d{11}$/).required(),
     birthday: joi.date().required()
 })
--- src/schemas/games.schema.js
+++ src/schemas/games.schema.js
@@ -3,6 +3,6 @@ import joi from 'joi';
 export const registerGameSchema = joi.object({
     name: joi.string().required().trim(),
     image: joi.string().trim(),
-    stockTotal: joi.number().min(0).required(),
-    pricePerDay: joi.number().min(0).required()
+    stockTotal: joi.number().min(1).required(),
+    pricePerDay: joi.number().min(1).required()
 })
